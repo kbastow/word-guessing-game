d3ace716f087b7eb0c089514e267156b4acd4e1c
kbastow <bastowkate@gmail.com>
2026-08-15 20:27:37 +1000

    feat: add confirmation check to restart game button

--- src/components/Controls/RestartButton.tsx
+++ src/components/Controls/RestartButton.tsx
@@ -1,14 +1,46 @@
-import React from "react";
-import { Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 interface RestartButtonProps {
   onRestart: () => void;
 }
 
-const RestartButton: React.FC<RestartButtonProps> = ({ onRestart }) => (
-  <Button variant="outlined" onClick={onRestart}>
-    Restart Game
-  </Button>
-);
+const RestartButton: React.FC<RestartButtonProps> = ({ onRestart }) => {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const handleConfirm = () => {
+    setConfirmOpen(false);
+    onRestart();
+  };
+
+  return (
+    <>
+      <Button variant="outlined" onClick={() => setConfirmOpen(true)}>
+        Restart Game
+      </Button>
+      <Dialog open={confirmOpen} onClose={() => setConfirmOpen(false)}>
+        <DialogTitle>Restart?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Your current score will be lost.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setConfirmOpen(false)}>Cancel</Button>
+          <Button onClick={handleConfirm} autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
 
 export default RestartButton;
